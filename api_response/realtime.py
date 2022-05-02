5829e7c21bc1d1de2b997ed4126c388ccd9507d2
OverLeo007 <sokolov_lev2003@mail.ru>
2022-05-02 22:30:03 +0700

    realtime response changed

--- api_response/realtime.py
+++ api_response/realtime.py
@@ -29,26 +29,22 @@ def grab_notes(uid, lang):
     res_text = {'info': {}, 'characters': []}
 
     info = res_text['info']
-    info['dailik'] = f"{trans['completed_commissions'][lang]}" \
-                     f"{response['completed_commissions']}\\" \
+    info['dailik'] = f"{response['completed_commissions']}\\" \
                      f"{response['total_commissions']}"
 
     is_claimed = trans['claimed'][lang] if response['claimed_commission_reward'] is True else trans['not_claimed'][lang]
-    info['reward'] = f"{trans['claimed_commission_reward'][lang]}{is_claimed}"
-    info['bosses'] = f"{trans['remaining_boss_discounts'][lang]}" \
-                     f"{response['remaining_boss_discounts']}\\{response['max_boss_discounts']}"
+    info['reward'] = f"{is_claimed}"
+    info['bosses'] = f"{response['remaining_boss_discounts']}\\{response['max_boss_discounts']}"
 
-    info['resin'] = f"{trans['resin'][lang]}" \
-                    f"{response['resin']}\\{response['max_resin']}\n" \
-                    f"{trans['until_resin_limit'][lang]}" \
-                    f"{get_time_from_sec(response['until_resin_limit'], lang)}"
+    info['resin'] = {'amount': f"{response['resin']}\\{response['max_resin']}\n",
+                     'time': f"{get_time_from_sec(response['until_resin_limit'], lang)}"}
 
-    info['expedition'] = f"{trans['expeditions'][lang]}" \
-                         f"{len(list(filter(lambda expa: expa['remaining_time'] != 0, response['expeditions'])))}" \
+    info['expedition'] = f"{len(list(filter(lambda expa: expa['remaining_time'] != 0, response['expeditions'])))}" \
                          f"\\{response['max_expeditions']}"
 
     for exp in response['expeditions']:
         res_text['characters'].append({'img': get_img_from_web(exp['icon']),
+        # res_text['characters'].append({'img': exp['icon'],
                                        'time': f"{trans['remaining_time'][lang]}"
                                                f"{get_time_from_sec(exp['remaining_time'], lang)}"})
 
